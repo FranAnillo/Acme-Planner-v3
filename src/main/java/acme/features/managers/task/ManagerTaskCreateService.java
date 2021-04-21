7e90224f217976d6105b6dba341727424e21c3c6
eugvicvaz <eugvicvaz@alum.us.es>
2021-04-21 12:28:15 +0200

    008-Create tasks and save them. #10

---
+++ src/main/java/acme/features/managers/task/ManagerTaskCreateService.java
@@ -0,0 +1,100 @@
+package acme.features.managers.task;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.tasks.Task;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Manager;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class ManagerTaskCreateService implements AbstractCreateService<Manager, Task> {
+	
+	// Internal state 
+
+		@Autowired
+		protected ManagerTaskRepository repository;
+
+		// AbstractCreateService<Manager, Task> interface 
+
+		@Override
+		public boolean authorise(final Request<Task> request) {
+			assert request != null;
+
+			return true;
+		}
+
+		@Override
+		public void bind(final Request<Task> request, final Task entity, final Errors errors) {
+			assert request != null;
+			assert entity != null;
+			assert errors != null;
+
+			request.bind(entity, errors);
+		}
+
+		@Override
+		public void unbind(final Request<Task> request, final Task entity, final Model model) {
+			assert request != null;
+			assert entity != null;
+			assert model != null;
+
+			request.unbind(entity, model, "title", "description", "link");
+		}
+
+		@Override
+		public Task instantiate(final Request<Task> request) {
+			assert request != null;
+
+			Task result;
+			LocalDateTime start;
+			LocalDateTime end;
+
+			start = LocalDateTime.of(2021, Month.MAY, 2, 10, 0);
+			end = LocalDateTime.of(2021, Month.MAY, 8, 14, 0);
+
+			result = new Task();
+			result.setTitle("Task 1");
+			result.setDescription("Description of the taks 2");
+			result.setStart(start);
+			result.setEnd(end);
+			result.setLink("http://example.org");
+
+			return result;
+		}
+
+		@Override
+		public void validate(final Request<Task> request, final Task entity, final Errors errors) {
+			assert request != null;
+			assert entity != null;
+			assert errors != null;
+
+		}
+
+		@Override
+		public void create(final Request<Task> request, final Task entity) {
+			assert request != null;
+			assert entity != null;
+
+			LocalDateTime start;
+			LocalDateTime end;
+
+			start = LocalDateTime.of(2021, Month.MAY, 2, 10, 0);
+			end = LocalDateTime.of(2021, Month.MAY, 8, 14, 0);
+			
+			entity.setStart(start);
+			entity.setEnd(end);
+			this.repository.save(entity);
+		}
+	
+	
+	
+	
+
+}
