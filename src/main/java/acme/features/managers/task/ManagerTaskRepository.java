5712e718f84964e47700cc71b37daf66666b04a0
Alejandro Sevillano <Alejandro Sevillano@DESKTOP-RIF95CG>
2021-05-04 20:08:06 +0200

    Update hecho

--- src/main/java/acme/features/managers/task/ManagerTaskRepository.java
+++ src/main/java/acme/features/managers/task/ManagerTaskRepository.java
@@ -1,16 +1,33 @@
 package acme.features.managers.task;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import acme.entities.tasks.Task;
+import acme.entities.roles.Manager;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
 public interface ManagerTaskRepository extends AbstractRepository {
+	
+	
 
-	void save(Task task);
 	
-//	@Query("select task from Task task where task.manager.id=?1")
-//	Collection<Task> findMany(int idManager);
-	
+//@Query("select task from Task task where task.manager.id=?1")
+//Collection<Task> findMany(int idManager);
+//
+//@Query("select w from Manager w where w.userAccount.id = ?1")
+//Manager findOneManagerByUserAccountId(int id);
+
+@Query("select p from Provider p where p.userAccount.id = ?1")
+Manager findOneProviderByUserAccountId(int id);
+
+@Query("select ua from UserAccount ua where ua.id = ?1")
+Manager findOneManagerbyUserAccountById(int id);
+
+
+
+
+
+
+
 }
