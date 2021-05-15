415634d87fe7f4c3a57770c9d49723db8c313779
FranAnillo <fraanicar@alum.us.es>
2021-05-15 13:42:12 +0200

    task delete service and list service

--- src/main/java/acme/features/managers/task/ManagerTaskRepository.java
+++ src/main/java/acme/features/managers/task/ManagerTaskRepository.java
@@ -1,9 +1,12 @@
 package acme.features.managers.task;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.roles.Manager;
+import acme.entities.tasks.Task;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -24,10 +27,9 @@ Manager findOneProviderByUserAccountId(int id);
 @Query("select ua from UserAccount ua where ua.id = ?1")
 Manager findOneManagerbyUserAccountById(int id);
 
+@Query("select tasks from Task where tasks.manager.id =?1")
+List<Task> findManyTasksByManager(int workerId);
 
-
-
-
-
-
+@Query("select p from Task p where p.id= ?1")
+Task findOneTaskById(int taskId);
 }
