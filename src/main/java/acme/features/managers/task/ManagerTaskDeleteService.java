916244721de6dcde3e5c0e7ed17c7ef80a729ac6
FranAnillo <fraanicar@alum.us.es>
2021-05-16 22:48:11 +0200

    Manager task

--- src/main/java/acme/features/managers/task/ManagerTaskDeleteService.java
+++ src/main/java/acme/features/managers/task/ManagerTaskDeleteService.java
@@ -1,85 +1,118 @@
 package acme.features.managers.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import acme.entities.roles.Manager;
 import acme.entities.tasks.Task;
 import acme.framework.components.Errors;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
-import acme.framework.entities.Principal;
 import acme.framework.services.AbstractDeleteService;
 
 public class ManagerTaskDeleteService implements AbstractDeleteService<Manager,Task>{
-	// Internal state ---------------------------------------------------------
-
-		@Autowired
-		protected ManagerTaskRepository repository;
 
 	@Override
 	public boolean authorise(final Request<Task> request) {
-		assert request != null;
-		
-		final boolean result;
-		int taskId;
-		Task task;
-		Manager manager;
-		final Principal principal;
-		
-		taskId = request.getModel().getInteger("id");
-		task = this.repository.findOneTaskById(taskId);
-		manager = task.getManager();
-		principal = request.getPrincipal();
-		result= manager.getUserAccount().getId() == principal.getAccountId();
-		
-		return result;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public void bind(final Request<Task> request, final Task entity, final Errors errors) {
-		assert request != null;
-		assert entity != null;
-		assert errors != null;
-
-		request.bind(entity, errors);
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void unbind(final Request<Task> request, final Task entity, final Model model) {
-		assert request != null;
-		assert entity != null;
-		assert model != null;
+		// TODO Auto-generated method stub
 		
-		request.unbind(entity, model, "title", "description", "start", "end");
-		request.unbind(entity, model, "link", "publica", "finish", "workload");
-
 	}
 
 	@Override
 	public Task findOne(final Request<Task> request) {
-		assert request != null;
-		
-		Task result;
-		int id;
-		
-		id = request.getModel().getInteger("id");
-		result= this.repository.findOneTaskById(id);
-		return result;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public void validate(final Request<Task> request, final Task entity, final Errors errors) {
-		assert request != null;
-		assert entity != null;
-		assert errors != null;
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void delete(final Request<Task> request, final Task entity) {
-		assert request != null;
-		assert entity != null;
-
-		this.repository.delete(entity);
+		// TODO Auto-generated method stub
+		
 	}
+//	// Internal state ---------------------------------------------------------
+//
+//		@Autowired
+//		protected ManagerTaskRepository repository;
+//
+//	@Override
+//	public boolean authorise(final Request<Task> request) {
+//		assert request != null;
+//		
+//		final boolean result;
+//		int taskId;
+//		Task task;
+//		Manager manager;
+//		final Principal principal;
+//		
+//		taskId = request.getModel().getInteger("id");
+//		task = this.repository.findOneTaskById(taskId);
+////		manager = task.getManager();
+//		principal = request.getPrincipal();
+////		result= manager.getUserAccount().getId() == principal.getAccountId();
+//		
+////		return result;
+//	}
+//
+//	@Override
+//	public void bind(final Request<Task> request, final Task entity, final Errors errors) {
+//		assert request != null;
+//		assert entity != null;
+//		assert errors != null;
+//
+//		request.bind(entity, errors);
+//	}
+//
+//	@Override
+//	public void unbind(final Request<Task> request, final Task entity, final Model model) {
+//		assert request != null;
+//		assert entity != null;
+//		assert model != null;
+//		
+//		request.unbind(entity, model, "title", "description", "start", "end");
+//		request.unbind(entity, model, "link", "publica", "finish", "workload");
+//
+//	}
+//
+//	@Override
+//	public Task findOne(final Request<Task> request) {
+//		assert request != null;
+//		
+//		Task result;
+//		int id;
+//		
+//		id = request.getModel().getInteger("id");
+//		result= this.repository.findOneTaskById(id);
+//		return result;
+//	}
+//
+//	@Override
+//	public void validate(final Request<Task> request, final Task entity, final Errors errors) {
+//		assert request != null;
+//		assert entity != null;
+//		assert errors != null;
+//	}
+//
+//	@Override
+//	public void delete(final Request<Task> request, final Task entity) {
+//		assert request != null;
+//		assert entity != null;
+//
+//		this.repository.delete(entity);
+//	}
 
 }
--- src/main/java/acme/features/managers/task/ManagerTaskRepository.java
+++ src/main/java/acme/features/managers/task/ManagerTaskRepository.java
@@ -1,7 +1,5 @@
 package acme.features.managers.task;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -27,8 +25,8 @@ Manager findOneProviderByUserAccountId(int id);
 @Query("select ua from UserAccount ua where ua.id = ?1")
 Manager findOneManagerbyUserAccountById(int id);
 
-@Query("select tasks from Task where tasks.manager.id =?1")
-List<Task> findManyTasksByManager(int workerId);
+//@Query("select tasks from Task where tasks.manager.id =?1")
+//List<Task> findManyTasksByManager(int workerId);
 
 @Query("select p from Task p where p.id= ?1")
 Task findOneTaskById(int taskId);
