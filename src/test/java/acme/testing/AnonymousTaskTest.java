1099f2a3dc1b64231cdd4921d3b2d22d82304abf
FranAnillo <fraanicar@alum.us.es>
2021-05-17 02:20:37 +0200

    Various creates

--- src/test/java/acme/testing/AnonymousTaskTest.java
+++ src/test/java/acme/testing/AnonymousTaskTest.java
@@ -15,23 +15,23 @@ public class AnonymousTaskTest extends AcmePlannerTest{
 		super.clickOnMenu("Anonymous", "Tasks");		
 		
 		super.checkColumnHasValue(recordIndex, 0, title);
-		super.checkColumnHasValue(recordIndex, 1, description);
-		super.checkColumnHasValue(recordIndex, 2, start);
-		super.checkColumnHasValue(recordIndex, 3, end);
-		super.checkColumnHasValue(recordIndex, 4, workload);
-
+		super.checkColumnHasValue(recordIndex, 1, workload);
+		super.checkColumnHasValue(recordIndex, 2, description);
+		super.checkColumnHasValue(recordIndex, 3, start);
+		super.checkColumnHasValue(recordIndex, 4, end);
+		
 		super.clickOnListingRecord(recordIndex);
 		
 		super.checkInputBoxHasValue("title", title);	
 		super.checkInputBoxHasValue("description",description);	
 		super.checkInputBoxHasValue("start",start);
 		super.checkInputBoxHasValue("end",end);
-		super.checkInputBoxHasValue("link", link);
-		super.checkInputBoxHasValue("publica", publica);
-		super.checkInputBoxHasValue("finish",finish);
-		super.checkInputBoxHasValue("workload",workload);
+	//	super.checkInputBoxHasValue("link", link);
+	//	super.checkInputBoxHasValue("publica", publica);
+	//	super.checkInputBoxHasValue("finish",finish);
+	//	super.checkInputBoxHasValue("workload",workload);
 
-		super.signOut();
+
 		
 	}
 	
