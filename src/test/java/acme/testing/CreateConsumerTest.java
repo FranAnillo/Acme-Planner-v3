8eb00fed99a5594f2978a0bbcd1867fc748a23f9
eugvicvaz <eugvicvaz@alum.us.es>
2021-05-25 23:40:09 +0200

    comentario en consumer test

--- src/test/java/acme/testing/CreateConsumerTest.java
+++ src/test/java/acme/testing/CreateConsumerTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CreateConsumerTest extends AcmePlannerTest {
+	
+	//Este metodo prueba un caso negativo de crear un consumidor
+	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/consumer/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)	
@@ -24,7 +27,7 @@ public class CreateConsumerTest extends AcmePlannerTest {
 		super.signOut();
 	}	
 	
-	
+	//Este metodo prueba un caso positivo de crear un consumidor
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/consumer/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
