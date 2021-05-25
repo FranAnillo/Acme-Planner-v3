620f3bf645b1d36c3a20b5ac74348d68de298502
Alvaro <alvalcgra@alum.us.es>
2021-05-25 23:39:34 +0200

    comentarios de los test

--- src/test/java/acme/testing/ManagerCreateTask.java
+++ src/test/java/acme/testing/ManagerCreateTask.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class ManagerCreateTask extends AcmePlannerTest {
-	
+	//Este metodo prueba que se crea correctamente una task del manager con los datos pertinentes
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)	
@@ -27,7 +27,7 @@ public class ManagerCreateTask extends AcmePlannerTest {
 		super.checkSimplePath("/master/welcome");
 		super.signOut();
 	}
-	
+	//Este metodo prueba que no se crea correctamente una task del manager por meter de forma incorrecta algun dato
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)	
