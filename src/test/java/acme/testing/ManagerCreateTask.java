262f0ce04c0a77a8a389c420d652d1fb47f015f6
Daniel Dorante <dandorsan@alum.us.es>
2021-05-25 21:16:53 +0200

    TestManager

--- src/test/java/acme/testing/ManagerCreateTask.java
+++ src/test/java/acme/testing/ManagerCreateTask.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class ManagerCreateTask extends AcmePlannerTest {
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/authenticated/task.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/manager/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)	
 	public void create(final String title, final String description,final String workload,final String start, final String end, final String link) {		
-		super.signIn("manager1", "manager1");
-		super.clickOnMenu("Manager", "Create Task");		
+		
+		super.signIn("administrator", "administrator");
+		super.clickOnMenu("Account", "Become a Manager");
+		super.clickOnMenu("Manager", "Tasks");	
 		
 		super.fillInputBoxIn("title", title);
 		super.fillInputBoxIn("description", description);
@@ -23,6 +25,28 @@ public class ManagerCreateTask extends AcmePlannerTest {
 		super.clickOnSubmitButton("Create");
 		super.checkSimplePath("/manager/task");
 		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)	
+	public void createNegative(final String title, final String description,final String workload,final String start, final String end, final String link) {
+		
+		super.signIn("administrator", "administrator");
+		super.clickOnMenu("Account", "Become a Manager");
+		super.clickOnMenu("Manager", "Tasks");	
+		
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("workload",workload);
+		super.fillInputBoxIn("start",start);
+		super.fillInputBoxIn("end",end);
+		super.fillInputBoxIn("workload",workload);
+		super.fillInputBoxIn("link",link);
+		super.clickOnSubmitButton("Create");
+		super.checkSimplePath("/manager/task");
+		super.signOut();
+
 	}
 
 }
