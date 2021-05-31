dd8fa7741152789756b63b1b107fb30c52a6c143
FranAnillo <fraanicar@alum.us.es>
2021-05-31 20:06:21 +0200

    bug fix

--- src/main/java/acme/entities/workplans/Workplan.java
+++ src/main/java/acme/entities/workplans/Workplan.java
@@ -1,59 +1,59 @@
-//package acme.entities.workplans;
-//
-//import java.time.LocalDateTime;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.ManyToOne;
-//import javax.validation.Valid;
-//import javax.validation.constraints.Future;
-//import javax.validation.constraints.NotNull;
-//
-//import acme.entities.roles.Manager;
-//import acme.entities.tasks.Task;
-//import acme.framework.entities.DomainEntity;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class Workplan extends DomainEntity {
-//
-//	// Serialisation identifier -----------------------------------------------
-//
-//	protected static final long	serialVersionUID	= 1L;
-//	
-//	// Attributes -------------------------------------------------------------
-//	
-//	@NotNull
-//	protected String title;
-//	
-//	@Future                       
-//	@NotNull                      
-//	protected LocalDateTime start;
-//
-//	@Future                     
-//	@NotNull                    
-//	protected LocalDateTime end;
-//
-//	@NotNull                   
-//	protected Integer workload;
-//	
-//	@NotNull
-//	protected Boolean finish;
-//	
-//	@NotNull
-//	protected Boolean publica;
-//	
-//	// Relationships ----------------------------------------------------------
-//	
-//	@NotNull
-//	@Valid
-//	@ManyToOne(optional = false)
-//	protected Manager manager;
-//	
-//	@NotNull
-//	@Valid
-//	@ManyToOne(optional = false)
-//	protected Task task;
-//}
+package acme.entities.workplans;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+
+import acme.entities.roles.Manager;
+import acme.entities.tasks.Task;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Workplan extends DomainEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+	
+	// Attributes -------------------------------------------------------------
+	
+	@NotNull
+	protected String title;
+	
+	@Future                       
+	@NotNull                      
+	protected LocalDateTime start;
+
+	@Future                     
+	@NotNull                    
+	protected LocalDateTime end;
+
+	@NotNull                   
+	protected Integer workload;
+	
+	@NotNull
+	protected Boolean finish;
+	
+	@NotNull
+	protected Boolean publica;
+	
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Manager manager;
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Task task;
+}
