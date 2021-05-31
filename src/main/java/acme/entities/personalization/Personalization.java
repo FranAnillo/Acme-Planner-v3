9c9c56a7d0422e0d1de3396ca35206b56862d153
FranAnillo <fraanicar@alum.us.es>
2021-05-31 20:09:02 +0200

    bug fixing

--- src/main/java/acme/entities/personalization/Personalization.java
+++ src/main/java/acme/entities/personalization/Personalization.java
@@ -8,18 +8,18 @@ import javax.validation.constraints.NotNull;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
 import lombok.Setter;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class Personalization extends DomainEntity{
-//
-//	
-//	private static final long serialVersionUID = 1L;
-//
-//	@NotNull
-//	protected List<String> censoredWords;
-//	
-//	@NotNull
-//	protected int umbral;
-//}
+
+@Entity
+@Getter
+@Setter
+public class Personalization extends DomainEntity{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@NotNull
+	protected List<String> censoredWords;
+	
+	@NotNull
+	protected int umbral;
+}
--- src/main/java/acme/features/administrator/personalization/AdministratorPersonalizationController.java
+++ src/main/java/acme/features/administrator/personalization/AdministratorPersonalizationController.java
@@ -1,11 +1,16 @@
 package acme.features.administrator.personalization;
 
-//@Controller
-//@RequestMapping("/administrator/personalization/")
-//public class AdministratorPersonalizationController {
-//
-//	@PostConstruct
-//	protected void initialise() {
-//		
-//	}
-//}
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administrator/personalization/")
+public class AdministratorPersonalizationController {
+
+	@PostConstruct
+	protected void initialise() {
+		
+	}
+}
