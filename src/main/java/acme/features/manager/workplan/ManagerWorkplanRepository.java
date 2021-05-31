685a5b725ba14b765f491df3d9feb600e4da9f56
FranAnillo <fraanicar@alum.us.es>
2021-05-31 11:32:47 +0200

    Manager Workplan show service

--- src/main/java/acme/features/manager/workplan/ManagerWorkplanRepository.java
+++ src/main/java/acme/features/manager/workplan/ManagerWorkplanRepository.java
@@ -18,7 +18,7 @@ public interface ManagerWorkplanRepository extends AbstractRepository {
 	Manager findOneManagerbyUserAccountById(int id);
 	
 	@Query("select w from Workplan w where w.id= ?1")
-	Workplan findOneTaskById(int workplanId);
+	Workplan findOneWorkplanById(int workplanId);
 	
 	@Query("select w from Workplan w")
 	Collection<Workplan> findMany();
